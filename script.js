a04c9104873d3a21313c952c80587dc0a8b828fa
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-19 19:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let myPromise;
+function check() {
+	let age = document.getElementById("age").value
+	let name = document.getElementById("name").value
+	if (age.length==0 || name.length==0) {
+		return "inputs cannot be empty"
+	}
+	else{
+		 myPromise = new Promise((resolve,reject)=>{
+			 setTimeout(()=>{
+				 if (parseInt(age)>18) {
+				 	resolve(`Welcome, ${name}. You can vote.`)
+				 } else {
+				 	reject(`Oh sorry ${name}. You aren't old enough.`)
+				 }
+			 },4000)
+		})
+	}
+}
+myPromise.then((res)=>{
+	alert(res)
+}).catch((err)=>alert(err))
